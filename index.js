f1eaded4029e6d781c6c499c2e2dbe57315b13f0
amelki <240f0c53eac357cd1ff1c02f9d05e376d407dc5e>
2017-03-13 17:52:15 +0100

    Implemented /account

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ module.exports.account = function (oauthToken) {
 		};
 		console.log('Getting account details for: ' + axios.defaults.baseURL);
 		getShop().subscribe(res => {
+			console.log(util.inspect("Got Shop: " + res));
 			resolve({
 				loginName: res.shop.name,
 				accountUrl: 'https://' + res.shop.domain
