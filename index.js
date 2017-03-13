e7ec584e382f9db86fe0e70172386a82b5f76d00
amelki <240f0c53eac357cd1ff1c02f9d05e376d407dc5e>
2017-03-13 09:41:59 +0100

    Fixed oauth URL

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ module.exports.authorize = function (params, options) {
 	return new Promise(function (resolve, reject) {
 		const shop = params.shop;
 		const code = params.code;
-		axios.post(shop + '/admin/oauth/access_token', {
+		axios.post('https://' + shop + '/admin/oauth/access_token', {
 			client_id: options.clientId,
 			client_secret: options.clientSecret,
 			code: code
